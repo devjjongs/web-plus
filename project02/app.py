dde3aa369dad52f29d7e41c5521c4e4848dfb835
devjjongs <84493338+devjjongs@users.noreply.github.com>
2022-05-10 15:21:37 +0900

    나만의 단어장 - ajax로 가져오기
    owl api를 이용하여 단어를 검색하고 ajax를 이용해 가져오는  연습

--- project02/app.py
+++ project02/app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, jsonify, redirect, url_for
 from pymongo import MongoClient
 import requests
 
-
 app = Flask(__name__)
 
 client = MongoClient('3.35.233.245', 27017, username="test", password="test")
@@ -10,25 +9,25 @@ db = client.dbsparta_plus_week2
 
 
 @app.route('/')
-def main():
+def main ():
     # DB에서 저장된 단어 찾아서 HTML에 나타내기
     return render_template("index.html")
 
 
 @app.route('/detail/<keyword>')
-def detail(keyword):
+def detail (keyword):
     # API에서 단어 뜻 찾아서 결과 보내기
     return render_template("detail.html", word=keyword)
 
 
 @app.route('/api/save_word', methods=['POST'])
-def save_word():
+def save_word ():
     # 단어 저장하기
     return jsonify({'result': 'success', 'msg': '단어 저장'})
 
 
 @app.route('/api/delete_word', methods=['POST'])
-def delete_word():
+def delete_word ():
     # 단어 삭제하기
     return jsonify({'result': 'success', 'msg': '단어 삭제'})
 
