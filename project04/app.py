3cd14826ca668665fb25200592594f8905e1c43b
devjjongs <84493338+devjjongs@users.noreply.github.com>
2022-05-11 15:30:27 +0900

    메인 페이지 기능 만들기 - 좋아요
    -좋아요/좋아요 취소 기능 만들기
    -좋아요 숫자 표시하기
    -JS 파일 분리하기

--- project04/app.py
+++ project04/app.py
@@ -137,10 +137,17 @@ def get_posts ():
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
         # 포스팅 목록 받아오기
-        posts = list(db.posts.find({}).sort("date", -1).limit(20))  # 포스트 전체 목록증 최근순으로 20개만 가져오는 코드
+        # 포스트 전체 목록증 최근순으로 20개만 가져오는 코드
+        posts = list(db.posts.find({}).sort("date", -1).limit(20))
         for post in posts:
-            post["_id"] = str(post["_id"])  # 포스트 작성한 id를 문자열로 바꿔준다.
-        return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다.", "posts":posts})
+            # 포스트 작성한 id를 문자열로 바꿔준다.
+            post["_id"] = str(post["_id"])
+            post["count_heart"] = db.likes.count_documents(
+                {"post_id": post["_id"], "type": "heart"})  # 해당 글의 좋아요 숫자를 카운트
+            post["heart_by_me"] = bool(
+                db.likes.find_one(
+                    {"post_id": post["_id"], "type": "heart", "username": payload['id']}))  # 내가 좋아요를 눌렀는지 확인
+        return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다.", "posts": posts})
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
 
@@ -151,7 +158,21 @@ def update_like ():
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
         # 좋아요 수 변경
-        return jsonify({"result": "success", 'msg': 'updated'})
+        user_info = db.users.find_one({"username": payload["id"]})
+        post_id_receive = request.form["post_id_give"]
+        type_receive = request.form["type_give"]
+        action_receive = request.form["action_give"]
+        doc = {
+            "post_id": post_id_receive,
+            "username": user_info["username"],
+            "type": type_receive
+        }
+        if action_receive == "like":
+            db.likes.insert_one(doc)
+        else:
+            db.likes.delete_one(doc)
+        count = db.likes.count_documents({"post_id": post_id_receive, "type": type_receive})
+        return jsonify({"result": "success", 'msg': 'updated', "count": count})
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
 
